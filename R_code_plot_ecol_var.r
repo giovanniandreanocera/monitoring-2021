a427b667b6bfb326eadd0c3cf179206d3a9cc231
giovannianocera <73804318+giovannianocera@users.noreply.github.com>
2020-11-08 22:40:52 +0100

    Create R_code_plot_ecol_var.r

---
+++ R_code_plot_ecol_var.r
@@ -0,0 +1,16 @@
+# R code for plotting the relationship among ecological variables
+
+install.packages("sp")
+library(sp)
+
+# data is used to recall datasets
+data(meuse)
+meuse
+
+# View is used to invoke a spreadsheet-style data viewer on a matrix-like R object
+View(meuse)
+
+# head is used to returns the first or last parts of a vector, matrix, table, data frame or function
+head(meuse)
+
+ # Exercise: meaning of all of the variables
