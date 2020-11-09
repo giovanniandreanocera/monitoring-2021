ee09b8d210dec69b331f1e54ca1812d95083a5b8
giovannianocera <73804318+giovannianocera@users.noreply.github.com>
2020-11-09 14:46:04 +0100

    Update R_code_plot_ecol_var.r

--- R_code_plot_ecol_var.r
+++ R_code_plot_ecol_var.r
@@ -29,6 +29,36 @@ plot(cadmium, zinc)
 # how to impress your supervisor! Scatterplot Matrices
 pairs(meuse)
 
-# Questin!!!
+# Question!!!
 # pairing only the elements part of the dataset: how to do that?
 # only with cadmium, copper, lead, zinc...
+
+# Lecture #2 on ecological variables
+# cadmium copper lead zinc
+# pairs with soil variables
+# from column 3 to column 6
+pairs(meuse[,3:6])
+
+# Let's use the names of the columns:
+# tilde Alt + 126: ~
+pairs(~ cadmium + copper + lead + zinc, data=meuse)
+
+# let's prettify the graph
+# Excercise: just use cadmium, lead and zinc
+pairs(~ cadmium + lead + zinc, data=meuse)
+
+# Excercise: change the color [col=""]
+pairs(~ cadmium + lead + zinc, data=meuse, col="green")
+
+#Excercise: change the forms/symbols of the dataset; use a filled triangle [pch=]
+pairs(~ cadmium + lead + zinc, data=meuse, col="green", pch=17)
+
+# Excercise: increase the sizes of the triangles [cex=]
+pairs(~ cadmium + lead + zinc, data=meuse, col="green", pch=17, cex=3)
+
+# Excercise: write a name on top of the graph [main=""]
+pairs(~ cadmium + lead + zinc, data=meuse, col="green", pch=17, cex=3, main="my second ecograph")
+
+
+
+
