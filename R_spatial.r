09cfbfda9dc1bf96eb92b3d2993f2a230ef99308
giovannianocera <73804318+giovannianocera@users.noreply.github.com>
2020-11-16 11:00:59 +0100

    Update R_spatial.r

--- R_spatial.r
+++ R_spatial.r
@@ -26,3 +26,46 @@ bubble(meuse, "zinc")
 # Do the same for lead
 bubble(meuse, "lead")
 bubble(meuse, "lead", col="red")
+
+
+# Installing the ggplot2 library / package
+
+install.packages("ggplot2")
+library(ggplot2) # [if no warnings, everything is fine]
+
+# ecological dataframe {biofuels - oxydative enzimes}
+biofuels <- c(120, 200, 350, 570, 750) # array of values: c
+oxydative <- c(1200, 1300, 21000, 34000, 50000)
+
+# dataframe function 
+d <- data.frame(biofuels, oxydative)
+d
+
+# Using ggplot function [ggplot(data, aes (x =, y =))] 
+ggplot(d, aes (x = biofuels, y = oxydative))
+# geom_point()   function
+ggplot(d, aes (x = biofuels, y = oxydative)) + geom_point()
+# change the color of the points [col=""]
+ggplot(d, aes (x = biofuels, y = oxydative)) + geom_point(col="red")
+# change the size of the points [cex= or size=]
+ggplot(d, aes (x = biofuels, y = oxydative)) + geom_point(col="red", size=5)
+# change the forms of the points [pch=]
+ggplot(d, aes (x = biofuels, y = oxydative)) + geom_point(col="red", size=5, pch=10)
+
+# Change the points in lines [geom_lines()]
+ggplot(d, aes (x = biofuels, y = oxydative)) + geom_line()
+# Points + Lines
+ggplot(d, aes (x = biofuels, y = oxydative)) + geom_point(col="red", size=5, pch=10) + geom_line()
+
+# Polygons [geom_polygons()]
+ggplot(d, aes (x = biofuels, y = oxydative)) + geom_polygon()
+
+########### IMPORT DATA FRON AN EXTERNAL SOURCE
+
+# setwd("path/lab")
+
+# setwd for Windows
+# setwd("C:/..../lab/") C:\Users\giova\OneDrive\Desktop\lab\
+setwd("C:/Users/giova/OneDrive/Desktop/lab/")
+covid <- 
+
