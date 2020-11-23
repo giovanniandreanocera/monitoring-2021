71801a12cd5940bbc9b09651a34b3fc6fa368ce8
giovannianocera <73804318+giovannianocera@users.noreply.github.com>
2020-11-23 15:41:45 +0100

    Update R_code_point_pattern_analysis.r

--- R_code_point_pattern_analysis.r
+++ R_code_point_pattern_analysis.r
@@ -31,3 +31,50 @@ points(covid_planar)
 
 # Putting the countries 
 install.packages("rgdal")
+
+library(rgdal)
+
+# Recall coastline data in the (C:/lab/) folder
+coastline <- readOGR("ne_10m_coastline.shp")
+
+# cl <- colorRampPalette(c('green','blue','magenta','yellow'))(100)
+plot(density_map, col = cl)
+points(covid_planar, pch = 19, cex = 0.5)
+plot(coastline, add = TRUE) # add = TRUE is used to join the two set
+
+# Export maps png
+png("figure1.png")
+cl <- colorRampPalette(c('green','blue','magenta','yellow'))(100)
+plot(density_map, col = cl)
+points(covid_planar, pch = 19, cex = 0.5)
+plot(coastline, add = TRUE)
+devoff()
+
+# pdf
+pdf("figure1.pdf")
+cl <- colorRampPalette(c('green','blue','magenta','yellow'))(100)
+plot(density_map, col = cl)
+points(covid_planar, pch = 19, cex = 0.5)
+plot(coastline, add = TRUE)
+devoff()
+
+
+
+##########à
+head(covid)
+
+# interpolation
+marks(covid_planar) <- cases
+cases_map <- Smooth(covid_planar)
+    
+plot(cases_map, col = cl)
+points(covid_planar)
+plot(coastline, add = T)
+
+###### ERRORE
+#> plot(cases_map, col = cl)
+#> points(covid_planar)
+#> plot(coastline, add = T)
+#> plot(case_map)
+#Error in h(simpleError(msg, call)) : 
+#  error in evaluating the argument 'x' in selecting a method for function 'plot': oggetto "case_map" non trovato
