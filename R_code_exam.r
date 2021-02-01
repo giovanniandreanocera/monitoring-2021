2f469170269c9bcb2f6e2a5162db942d7b9ecb69
Giovanni <73804318+giovanniandreanocera@users.noreply.github.com>
2021-02-01 20:10:02 +0100

    Update R_code_exam.r

--- R_code_exam.r
+++ R_code_exam.r
@@ -10,7 +10,7 @@ library(ncdf4)
 library(raster)
 
 # Data collected from Copernicus: Leaf Area Index in the period between 1999-2019
-# Two dataset for each year: s=summer(july-august), w=winter(november-march)
+# Two dataset for each year: s=summer(june-october), w=winter(november-march)
 
 LAI_1999s <- raster("c_gls_LAI_199908100000_GLOBE_VGT_V2.0.2.nc")
 LAI_1999w <- raster("c_gls_LAI_200001100000_GLOBE_VGT_V2.0.2.nc")
@@ -59,12 +59,10 @@ Afr_LAI_2011 <- (Afr_LAI_2011s + Afr_LAI_2011w)/2
 Afr_LAI_2015 <- (Afr_LAI_2015s + Afr_LAI_2015w)/2
 Afr_LAI_2019 <- (Afr_LAI_2019s + Afr_LAI_2019w)/2
 
-# Better to use different colors
-# cl <- colorRampPalette(c('lightblue', 'blue', 'red', 'orange', 'yellow'))(100)
-# cl <- colorRampPalette(c('white', 'blue', 'red', 'orange', 'yellow'))(100)
+# Better to use different colors: we want to enlight (yellow) the low-medium values 
 cl <- colorRampPalette(c('slategray3', 'yellow', 'orange', 'red'))(100)
 
-# Using the par function to visualize all the graph at the same time
+# Using the par function to visualize all the graph at the same time 
 par(mfrow=c(3,2))
 plot(Afr_LAI_1999, col=cl, main="avg LAI 1999")
 plot(Afr_LAI_2003, col=cl, main="avg LAI 2003")
@@ -82,33 +80,76 @@ par(mfrow=c(2,1))
 plot(Afr_LAI_1999, col=cl, main="avg LAI 1999")
 plot(Afr_LAI_2019, col=cl, main="avg LAI 2019")
 
-# Using minus ( - ) function to find the differences between the two graphs
+# Using minus ( - ) function to find out the differences
 diff_20yr <- (Afr_LAI_2019 - Afr_LAI_1999)
 plot(diff_20yr)
 
-# Let’s change colors to evidentiate the differences: using red to represent the desertification
+# Let’s change colors to evidentiate the differences: using red to represent the desertification (lower LAI values)
 clmax <- colorRampPalette(c('red', 'white', 'blue'))(100)
+
 plot(diff_20yr, col=clmax, main="Variation from 1999 to 2019")
 
+# What would we see if confronting 2018 to 2019?
+LAI_2018s <- raster("c_gls_LAI-RT6_201808100000_GLOBE_PROBAV_V2.0.1.nc")
+LAI_2018w <- raster("c_gls_LAI-RT6_201812100000_GLOBE_PROBAV_V2.0.1.nc")
+# Zoom
+Afr_LAI_2018s <- crop(LAI_2018s, ext)
+Afr_LAI_2018w <- crop(LAI_2018w, ext)
+# Average
+Afr_LAI_2018 <- (Afr_LAI_2018s + Afr_LAI_2018w)/2
 
-
-#################################################à
-# These changes are related to differences in Albedo index in the area during this period?
-
-# Data collected from Copernicus: Albedo in the years 1999-2019
-
-ALB_1999s <- raster("c_gls_ALBH_199907240000_GLOBE_VGT_V1.4.1.nc")
-ALB_2019s <- raster("c_gls_ALBH_201907240000_GLOBE_PROBAV_V1.5.1.nc")
-
-Afr_ALB_1999s <- crop(ALB_1999s, ext)
-Afr_ALB_2019s <- crop(ALB_2019s, ext)
-
-clALB <- colorRampPalette(c('black', 'lightgoldenrod', 'yellow'))(100)
-
+# One above the other
 par(mfrow=c(2,1))
-plot(Afr_ALB_1999s, col=clALB)
-plot(Afr_ALB_2019s, col=clALB)
+plot(Afr_LAI_2018, col=cl, main="avg LAI 2018")
+plot(Afr_LAI_2019, col=cl, main="avg LAI 2019")
 
+# Differences between 2018 and 2019
+diff_B <- (Afr_LAI_2019 - Afr_LAI_2018)
+dev.off()
+plot(diff_B, col=clmax, main="Variation from 2018 to 2019")
+
+# Desertification of the Sahel region: it’s actually increasing?
+par(mfrow=c(2,1))
+plot(diff_20yr, col=clmax, main="Variation from 1999 to 2019")
+plot(diff_B, col=clmax, main="Variation from 2018 to 2019")
+
+
+# Albedo index is higher in desertic areas respect to plant-rich areas
+# Is there an increase in the Albedo index in the area during this period?
+# Data collected from Copernicus: Albedo in the years 1999-2019
+ALB_1999s <- raster("c_gls_ALBH_199907240000_GLOBE_VGT_V1.4.1.nc")
+ALB_1999w <- raster("c_gls_ALBH_200001130000_GLOBE_VGT_V1.4.1.nc")
+ALB_2019s <- raster("c_gls_ALBH_201907240000_GLOBE_PROBAV_V1.5.1.nc")
+ALB_2019w <- raster("c_gls_ALBH_202001130000_GLOBE_PROBAV_V1.5.1.nc")
+# Zoom
+Afr_ALB_1999s <- crop(ALB_1999s, ext)
+Afr_ALB_1999w <- crop(ALB_1999w, ext)
+Afr_ALB_2019s <- crop(ALB_2019s, ext)
+Afr_ALB_2019w <- crop(ALB_2019w, ext)
+# Average
+Afr_ALB_1999 <- (Afr_ALB_1999s + Afr_ALB_1999w)/2
+Afr_ALB_2019 <- (Afr_ALB_2019s + Afr_ALB_2019w)/2
+
+# Changing colors
+clALB <- colorRampPalette(c('black', 'yellow', 'white'))(100)
+
+# Confronting the Albedo index in the years 1999 and 2019: one above the other
+par(mfrow=c(2,1))
+plot(Afr_ALB_1999, col=clALB, main="avg Albedo 1999")
+plot(Afr_ALB_2019, col=clALB, main="avg Albedo 2019")
+
+# Differences in Albedo index between 1999 and 2019
 diff_ALB <- (Afr_ALB_2019s - Afr_ALB_1999s)
 
-plot(diff_ALB, col=clALB)
+# Using clmax would enlight in red the lowest values: invert the colors
+clmax2 <- colorRampPalette(c('blue', 'white', 'red'))(100)
+#clean
+dev.off()
+
+plot(diff_ALB, col=clmax2, main="Albedo: variation from 1999 to 2019")
+
+# Let’s put all together
+par(mfrow=c(2,1))
+plot(diff_20yr, col=clmax, main="LAI")
+plot(diff_ALB, col=clmax2, main="Albedo")
+title("Variation from 1999 to 2019", line = -1, outer = TRUE)
